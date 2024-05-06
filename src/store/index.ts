865f9cd59e7021262807f7cac0eb82854f8bddd6
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2024-05-06 20:57:20 -0300

    Agrego Middleware para obtener Usuarios del Local Storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from './users/slice'
 
+const persistanceLocalStorageMiddleware = (store) => (next) => (action) =>{
+    next(action)
+    localStorage.setItem('__redux__state__', JSON.stringify(store.getState()))
+}
+
 export const store = configureStore({
     reducer: {
-        users: usersReducer
-    }
-})
+        users: usersReducer,
+    },
+    middleware: (getDefaultMiddleware) => {
+
+        return getDefaultMiddleware().concat(persistanceLocalStorageMiddleware)
+      },
+});
 
 export type RootSate = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -1,18 +1,6 @@
 import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
 
-export type UserId = string;
-
-export interface User{
-    name: string;
-    email: string;
-    github: string
-}
-
-export interface UserWithId extends User{
-    id: UserId;
-}
-
-const initialState: UserWithId[] = [
+const DEFAULT_STATE = [
     {
         id: '1',
         name: 'midudev',
@@ -76,6 +64,23 @@ const initialState: UserWithId[] = [
     }
 ]
 
+export type UserId = string;
+
+export interface User{
+    name: string;
+    email: string;
+    github: string
+}
+
+export interface UserWithId extends User{
+    id: UserId;
+}
+
+const initialState: UserWithId[] = (() => {
+    const persistedState = localStorage.getItem("__redux__state__")
+    return persistedState ? JSON.parse(persistedState).users : DEFAULT_STATE
+})()
+
 export const usersSlice = createSlice({
     name:'users',
     initialState: initialState,
@@ -85,6 +90,7 @@ export const usersSlice = createSlice({
             return state.filter((user) => user.id !== id)
         }
     }
+    
 })
 
 export default usersSlice.reducer
